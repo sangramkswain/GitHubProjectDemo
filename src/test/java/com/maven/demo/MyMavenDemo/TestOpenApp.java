8b4232d09a31d2db3e014f6563791455d905a284
Sangram Keshari Swain - ERS <Sangram.Swain@hcl.com>
2017-12-27 12:34:17 +0530

    adding github project

---
+++ src/test/java/com/maven/demo/MyMavenDemo/TestOpenApp.java
@@ -0,0 +1,17 @@
+package com.maven.demo.MyMavenDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestOpenApp {
+	
+	
+	@Test
+	public void openApp() {
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.myhcl.com");
+	}
+
+}
